84a6fed7eed79ed0a5b4fd799f35f88afaee2c32
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-03-17 10:52:41 +0100

    pictures added

--- src/componets/MovieCard/MovieCard.tsx
+++ src/componets/MovieCard/MovieCard.tsx
@@ -1,5 +1,6 @@
 import styles from './MovieCard.module.css'
 import { MovieType } from '@/app/types/movie'
+import Image from 'next/image';
 
 type Props = {
     movie: MovieType
@@ -9,6 +10,7 @@ export default function MovieCard({ movie }: Props) {
 
     return (
         <div className={styles.movie_card}>
+            <Image className={styles.movie_image} src={movie.image_url} alt={movie.name} width={150} height={225} />
             <p>{movie.name}</p>
         </div >
     );
