92a54b73b2b02030735dddb2fa323a165aebb679
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-03-17 10:20:49 +0100

    Movie card added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import styles from './page.module.css'
+import MovieCard from '@/componets/MovieCard'
 
 export default function Home() {
   return (
---
+++ src/componets/MovieCard/MovieCard.tsx
@@ -0,0 +1,10 @@
+import styles from './MovieCard.module.css'
+
+export default function MovieCard() {
+
+    return (
+        <div className={styles.movie_card}>
+            <p>Movie Card</p>
+        </div >
+    );
+};
