20128f62655598a079faae4e1aaea08da5b592b8
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-03-17 11:51:50 +0100

    update

--- src/componets/MovieCard/MovieCard.tsx
+++ src/componets/MovieCard/MovieCard.tsx
@@ -25,8 +25,7 @@ export default function MovieCard({ movie }: Props) {
         <div className={!isHovered ? styles.movie_card : styles.movie_card_hover} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} >
 
             <div className={styles.container}>
-                <Image className={!isHovered ? styles.movie_image : styles.movie_image_hover} src={movie.image_url} alt={movie.name} width={!isHovered ? 150 : 75} height={!isHovered ? 220 : 110}
-                    fill={false} />
+                <Image className={!isHovered ? styles.movie_image : styles.movie_image_hover} src={movie.image_url} alt={movie.name} width={!isHovered ? 150 : 75} height={!isHovered ? 220 : 110} />
                 <div className={styles.movie_info}>
                     {isHovered && <p className={styles.movie_name}> {movie.name}</p>}
                     {isHovered && <p className={styles.movie_rating}> Rating: {movie.rating}</p>}
