16983eb801efb116e89bec20ac47ed31c9009c7f
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-03-17 11:38:50 +0100

    search added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import styles from './page.module.css'
 import top250 from '../../public/top250.json'
 import MovieList from '@/componets/MovieList';
+import { useState } from 'react';
 
 function GetMovies() {
   return top250;
@@ -10,15 +11,22 @@ function GetMovies() {
 
 export default function Home() {
 
-  const movies = GetMovies();
+  const [filter, setFilter] = useState('');
+
+  let movies = GetMovies();
 
   movies.sort((a, b) => {
     return a.rating - b.rating;
   });
 
+  movies = movies.filter((movie) => {
+    return movie.name.toLowerCase().includes(filter.toLowerCase());
+  });
+
   return (
     <div className={styles.page}>
-      <h1> Hello World </h1>
+      <h1> Search </h1>
+      <input type="text" value={filter} onChange={e => setFilter(e.target.value)} />
       {
         <MovieList movies={movies} />
       }
