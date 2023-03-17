2ea17cf54af955b93aff31eddd1bba7a6b6d4481
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-03-17 10:45:21 +0100

    movie list added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 'use client';
 
 import styles from './page.module.css'
-import MovieCard from '@/componets/MovieCard'
 import top250 from '../../public/top250.json'
-import { MovieType } from '@/app/types/movie'
+import MovieList from '@/componets/MovieList';
 
 function GetMovies() {
   return top250;
@@ -13,17 +12,11 @@ export default function Home() {
 
   const movies = GetMovies();
 
-  const movieCards = movies.map((movie: MovieType, index: number) => {
-    return (
-      <MovieCard key={index} movie={movie} />
-    )
-  })
-
   return (
     <div className={styles.page}>
       <h1> Hello World </h1>
       {
-        movieCards
+        <MovieList movies={movies} />
       }
     </div>
   )
