a86f37bff99ea60a735ba0239616797559f4013c
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-03-17 11:35:09 +0100

    update

--- src/componets/MovieCard/MovieCard.tsx
+++ src/componets/MovieCard/MovieCard.tsx
@@ -1,6 +1,9 @@
+'use client';
+
 import styles from './MovieCard.module.css'
 import { MovieType } from '@/app/types/movie'
 import Image from 'next/image';
+import { useState } from 'react';
 
 type Props = {
     movie: MovieType
@@ -8,10 +11,29 @@ type Props = {
 
 export default function MovieCard({ movie }: Props) {
 
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    }
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    }
+
     return (
-        <div className={styles.movie_card}>
-            <Image className={styles.movie_image} src={movie.image_url} alt={movie.name} width={150} height={225} />
-            <p> {movie.name}</p>
+        <div className={!isHovered ? styles.movie_card : styles.movie_card_hover} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} >
+
+            <div className={styles.container}>
+                <Image className={!isHovered ? styles.movie_image : styles.movie_image_hover} src={movie.image_url} alt={movie.name} width={!isHovered ? 150 : 75} height={!isHovered ? 220 : 110}
+                    fill={false} />
+                <div className={styles.movie_info}>
+                    {isHovered && <p className={styles.movie_name}> {movie.name}</p>}
+                    {isHovered && <p className={styles.movie_rating}> Rating: {movie.rating}</p>}
+                    {isHovered && <p className={styles.movie_year}> Year: {movie.year}</p>}
+                </div>
+            </div>
+            {isHovered && <p className={styles.movie_desc}> {movie.desc}</p>}
         </div >
     );
 };
