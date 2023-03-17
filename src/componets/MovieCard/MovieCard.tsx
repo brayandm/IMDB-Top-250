c2496659755dff96378f8bb410318ee307a61c81
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-03-17 11:58:47 +0100

    update css

--- src/componets/MovieCard/MovieCard.tsx
+++ src/componets/MovieCard/MovieCard.tsx
@@ -1,9 +1,6 @@
-'use client';
-
 import styles from './MovieCard.module.css'
 import { MovieType } from '@/app/types/movie'
 import Image from 'next/image';
-import { useState } from 'react';
 
 type Props = {
     movie: MovieType
@@ -11,28 +8,18 @@ type Props = {
 
 export default function MovieCard({ movie }: Props) {
 
-    const [isHovered, setIsHovered] = useState(false);
-
-    const handleMouseEnter = () => {
-        setIsHovered(true);
-    }
-
-    const handleMouseLeave = () => {
-        setIsHovered(false);
-    }
-
     return (
-        <div className={!isHovered ? styles.movie_card : styles.movie_card_hover} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} >
+        <div className={styles.movie_card}>
 
             <div className={styles.container}>
-                <Image className={!isHovered ? styles.movie_image : styles.movie_image_hover} src={movie.image_url} alt={movie.name} width={!isHovered ? 150 : 75} height={!isHovered ? 220 : 110} />
+                <Image className={styles.movie_image} src={movie.image_url} alt={movie.name} width={150} height={220} />
                 <div className={styles.movie_info}>
-                    {isHovered && <p className={styles.movie_name}> {movie.name}</p>}
-                    {isHovered && <p className={styles.movie_rating}> Rating: {movie.rating}</p>}
-                    {isHovered && <p className={styles.movie_year}> Year: {movie.year}</p>}
+                    <p className={styles.movie_name}> {movie.name}</p>
+                    <p className={styles.movie_rating}> Rating: {movie.rating}</p>
+                    <p className={styles.movie_year}> Year: {movie.year}</p>
                 </div>
             </div>
-            {isHovered && <p className={styles.movie_desc}> {movie.desc}</p>}
+            <p className={styles.movie_desc}> {movie.desc}</p>
         </div >
     );
 };
